6503052f132cdcff36a5721e022286b468467079
Francisco Prin <franciscoprin@gmail.com>
2023-04-13 11:58:07 -0600

    [DEV-5075] adding SSO TeamMate resource

--- sendgrid/resource_sendgrid_sso_teammate_test.go
+++ sendgrid/resource_sendgrid_sso_teammate_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
-	sendgrid "github.com/taharah/terraform-provider-sendgrid/sdk"
+	sendgrid "github.com/SpotOnInc/terraform-provider-sendgrid/sdk"
 )
 
 func TestAccSendgridSsoTeammateBasic(t *testing.T) {
