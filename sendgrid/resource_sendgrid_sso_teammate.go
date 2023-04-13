988d0f2488d46609e1b0d63f1fd3a074c1a259d7
Francisco Prin <franciscoprin@gmail.com>
2023-04-13 12:08:52 -0600

    Replacing the Taharah URL reference with SpotOnInc in the missing SSO template URLs.

--- sendgrid/resource_sendgrid_sso_teammate.go
+++ sendgrid/resource_sendgrid_sso_teammate.go
@@ -25,7 +25,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
-	sendgrid "github.com/taharah/terraform-provider-sendgrid/sdk"
+	sendgrid "github.com/SpotOnInc/terraform-provider-sendgrid/sdk"
 )
 
 func resourceSendgridSSOTeammate() *schema.Resource {
